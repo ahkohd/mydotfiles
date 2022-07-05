105771784cc5a3486ed5086ac91004d88fde1bca
Victor Aremu <victor.olorunbunmi@gmail.com>
2022-07-05 11:24:32 +0100

    nvim(toggleterm): change terminal direction

--- nvim/after/plugin/toggleterm.rc.lua
+++ nvim/after/plugin/toggleterm.rc.lua
@@ -2,5 +2,7 @@ local status, toggleterm = pcall(require, "toggleterm")
 if (not status) then return end
 
 toggleterm.setup({
-  open_mapping = [[<c-\>]]
+  open_mapping = [[<c-\>]],
+  direction = "vertical",
+  size = 50
 })
