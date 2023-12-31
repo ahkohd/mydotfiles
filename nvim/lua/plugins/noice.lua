a2cd309c2416efd580be891b3877ff45490bf4c7
Victor Aremu <victor.olorunbunmi@gmail.com>
2023-12-31 14:01:36 +0100

    feat: add noice

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,4 +1,4 @@
--- luachek: globals vim
+-- luacheck: globals vim
 
 return {
 	"folke/noice.nvim",
@@ -21,26 +21,22 @@ return {
 				},
 				progress = { enabled = false },
 			},
-			-- you can enable a preset for easier configuration
 			presets = {
 				bottom_search = true, -- use a classic bottom cmdline for search
-				command_palette = true, -- position the cmdline and popupmenu together
+				command_palette = false, -- position the cmdline and popupmenu together
 				long_message_to_split = true, -- long messages will be sent to a split
 				inc_rename = false, -- enables an input dialog for inc-rename.nvim
 				lsp_doc_border = false, -- add a border to hover docs and signature help
 			},
 			cmdline = {
 				view = "cmdline",
-				-- opts = {
-				-- 	position = { row = "35%", col = "50%" },
-				-- },
 				format = {
-					cmdline = { pattern = "^:", icon = ":", lang = "vim" },
-				},
-			},
-			popupmenu = {
-				opts = {
-					position = { row = "50%", col = "50%" },
+					cmdline = { pattern = "^:", icon = " 󰘳", lang = "vim" },
+					search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+					search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+					filter = { pattern = "^:%s*!", icon = " ", lang = "bash" },
+					lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = " ", lang = "lua" },
+					help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
 				},
 			},
 		})
@@ -49,13 +45,17 @@ return {
 			background_colour = "#000000",
 			render = "default",
 			stages = "static",
-			timeout = 3000,
-			top_down = false,
 			icons = {
 				ERROR = icons.error,
 				WARN = icons.warn,
 				INFO = icons.info,
 			},
 		})
+		vim.keymap.set("n", "<leader>nc", function()
+			require("noice").cmd("dismiss")
+		end)
+		vim.keymap.set("n", "<leader>nh", function()
+			require("noice").cmd("telescope")
+		end)
 	end,
 }
